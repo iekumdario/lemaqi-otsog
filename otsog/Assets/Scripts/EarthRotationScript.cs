d5f44fdfad4e25ee584b7d8ab19b68f4796b90d8
Daniel Martinez <dani3lmt@gmail.com>
2018-10-20 15:22:48 -0500

    Adding Earth and Hubble rotation to SelectMissionScene

---
+++ otsog/Assets/Scripts/EarthRotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotationScript : MonoBehaviour {
+	public float speed = 1f;
+
+	public float direction = 1f;
+	[HideInInspector]
+	public float directionChangeSpeed = 2f;
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (direction < 1f) {
+			direction += Time.deltaTime / (directionChangeSpeed / 2);
+		}*/
+
+		transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
+	}
+}
