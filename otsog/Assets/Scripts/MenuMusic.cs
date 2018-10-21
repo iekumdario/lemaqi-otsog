0109cc2372a824e1a77f3ad6b9bf2cc12cb2f340
Lex <lexxxiss@gmail.com>
2018-10-21 11:59:31 -0500

    Adding global music

---
+++ otsog/Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour {
+	private static MenuMusic instance = null;
+	public static MenuMusic Instance {
+		get { return instance; }
+	}
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+}
